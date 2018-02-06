dc1b02ee32209726ac756beb1d25780b8f8f7c76
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-02-06 17:00:59 +0530

    Create mm19.c

---
+++ mm19.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,b=1;
+    printf("enter the start number n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    b=b*i;
+    
+    
+    }
+    printf("\nThe odd number is: %d",b);
+
+}
